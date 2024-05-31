c1b4295852baf14df063bdafcc2c592547f3c6c6
Yurii_Yevchenko <yurievch@gmail.com>
2024-05-31 19:01:49 +0300

    контролированный инпут

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Search.module.scss';
 
-const Search = () => {
+const Search = ({ searchValue, setSearchValue }) => {
   return (
     <div className={styles.root}>
       <svg
@@ -14,7 +14,22 @@ const Search = () => {
         <path d="M31 28h-1.59l-.55-.55c1.96-2.27 3.14-5.22 3.14-8.45 0-7.18-5.82-13-13-13s-13 5.82-13 13 5.82 13 13 13c3.23 0 6.18-1.18 8.45-3.13l.55.55v1.58l10 9.98 2.98-2.98-9.98-10zm-12 0c-4.97 0-9-4.03-9-9s4.03-9 9-9 9 4.03 9 9-4.03 9-9 9z" />
         <path d="M0 0h48v48h-48z" fill="none" />
       </svg>
-      <input className={styles.input} placeholder="Пошук піци..."></input>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Пошук піци..."
+      ></input>
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue('')}
+          className={styles.clearIcon}
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+        </svg>
+      )}
     </div>
   );
 };
